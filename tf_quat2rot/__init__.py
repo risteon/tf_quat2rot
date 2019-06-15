1eb518e2a0fe7e0ee4b25e85ae79c78267255575
Christoph Rist <c.rist@posteo.de>
2019-06-15 17:46:22 +0200

    Remove version strings

--- tf_quat2rot/__init__.py
+++ tf_quat2rot/__init__.py
@@ -2,7 +2,6 @@
 
 __author__ = """Christoph Rist"""
 __email__ = 'c.rist@posteo.de'
-__version__ = '0.1.0'
 
 from .conversion import quaternion_to_rotation_matrix
 from .conversion import rotation_matrix_to_quaternion
--- tf_quat2rot/tests/__init__.py
+++ tf_quat2rot/tests/__init__.py
@@ -2,4 +2,3 @@
 
 __author__ = """Christoph Rist"""
 __email__ = 'c.rist@posteo.de'
-__version__ = '0.1.0'
