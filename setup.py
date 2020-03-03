d97e15207367d12399c0ba63fd201c418cbfbd19
Christoph Rist <c.rist@posteo.de>
2020-03-03 18:31:44 +0100

    Bump to v1.1 with quaternion multiplication

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tf_quat2rot",
-    version="1.0.4",
+    version="1.1.0",
     description="Convert between quaternion and rotation matrices in tensorflow.",
     url="http://github.com/risteon/tf_quat2rot",
     author="Christoph Rist",
